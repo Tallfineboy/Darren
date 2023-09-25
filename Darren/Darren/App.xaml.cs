b0bd4417778c646d8aee64083ea636522ccd9498
user <user@DESKTOP-MD9910N>
2023-09-25 12:55:13 +0100

    Add project files.

---
+++ Darren/Darren/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Darren
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new OnboardingPage();
+		}
+
+		protected override void OnStart()
+		{
+		   new OnboardingPage();
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ Darren/Darren/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Darren
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync(new RegisterationPage());
+		}
+
+		private void Button_Clicked_1(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync(new ForgotPasswordPage());
+		}
+
+		private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync( new Tac());
+		}
+
+		private void Button_Clicked_2(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
